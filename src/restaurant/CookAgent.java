59e07b07aa16722b984f7c7a218cd8faa0e9a386
Trevor Reed <trevorar@usc.edu>
2013-09-17 19:46:21 -0700

    Updated skeleton code for CookAgent

--- src/restaurant/CookAgent.java
+++ src/restaurant/CookAgent.java
@@ -14,19 +14,17 @@ import java.util.concurrent.Semaphore;
 //the HostAgent. A Host is the manager of a restaurant who sees that all
 //is proceeded as he wishes.
 public class CookAgent extends Agent {
-	static final int NTABLES = 4;//a global for the number of tables.
-	//Notice that we implement waitingCustomers using ArrayList, but type it
-	//with List semantics.
-	public List<CustomerAgent> waitingCustomers
-	= new ArrayList<CustomerAgent>();
-	public Collection<Table> tables;
-	//note that tables is typed with Collection semantics.
-	//Later we will see how it is implemented
+	
+	public List<Order> orders = new ArrayList<Order>();
+	
+	public enum OrderState { pending, cooking, cooked, finished };
+	
+	//Implement this map
+	//map(String, Food) foods;
 
 	private String name;
-	private Semaphore atTable = new Semaphore(0,true);
-	private enum hostState {free, seatingCustomer};
-	private hostState state = hostState.free;
+	private enum cookState {free, seatingCustomer};
+	private cookState state = cookState.free;
 	
 	public HostGui hostGui = null;
 
@@ -34,77 +32,36 @@ public class CookAgent extends Agent {
 		super();
 
 		this.name = name;
-		// make some tables
-		tables = new ArrayList<Table>(NTABLES);
-		for (int ix = 1; ix <= NTABLES; ix++) {
-			tables.add(new Table(ix));//how you add to a collections
-		}
-	}
-
-	public String getMaitreDName() {
-		return name;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public List getWaitingCustomers() {
-		return waitingCustomers;
+	public List getOrders() {
+		return orders;
 	}
 
-	public Collection getTables() {
-		return tables;
-	}
 	// Messages
 
-	public void msgIWantFood(CustomerAgent cust) {
-		waitingCustomers.add(cust);
-		stateChanged();
-	}
-
-	public void msgLeavingTable(CustomerAgent cust) {
-		for (Table table : tables) {
-			if (table.getOccupant() == cust) {
-				print(cust + " leaving " + table);
-				table.setUnoccupied();
-				stateChanged();
-			}
-		}
-	}
-
-	public void msgAtTable() {//from animation
-		//print("msgAtTable() called");
-		atTable.release();// = true;
-		stateChanged();
+	public void msgHereIsOrder(WaiterAgent w, String choice, int table) {
+		//STUB
 	}
 	
-	public void msgInLobby() {
-		if(state == hostState.seatingCustomer) {
-			state = hostState.free;
-			stateChanged();
-		}
+	public void msgFoodDone(Order o) {
+		//STUB
 	}
 
 	/**
 	 * Scheduler.  Determine what action is called for, and do it.
 	 */
 	protected boolean pickAndExecuteAnAction() {
-		/* Think of this next rule as:
-            Does there exist a table and customer,
-            so that table is unoccupied and customer is waiting.
-            If so seat him at the table.
-		 */
-		for (Table table : tables) {
-			if (!table.isOccupied()) {
-				if (!waitingCustomers.isEmpty()) {
-					if(state == hostState.free) {
-						seatCustomer(waitingCustomers.get(0), table);//the action
-						return true;//return true to the abstract agent to reinvoke the scheduler.
-					}
-				}
-			}
+		/*if(cooked orders) {
+			//STUB
 		}
+		if(pending orders) {
+			//STUB
+		}*/
 
 		return false;
 		//we have tried all our rules and found
@@ -114,29 +71,16 @@ public class CookAgent extends Agent {
 
 	// Actions
 
-	private void seatCustomer(CustomerAgent customer, Table table) {
-		state = hostState.seatingCustomer;
-		customer.msgSitAtTable(table.tableNumber);
-		DoSeatCustomer(customer, table);
-		try {
-			atTable.acquire();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		table.setOccupant(customer);
-		waitingCustomers.remove(customer);
-		hostGui.DoLeaveCustomer();
+	private void cookIt(Order o) {
+		//STUB
+	}
+	
+	private void plateIt(Order o) {
+		//STUB
 	}
 
 	// The animation DoXYZ() routines
-	private void DoSeatCustomer(CustomerAgent customer, Table table) {
-		//Notice how we print "customer" directly. It's toString method will do it.
-		//Same with "table"
-		print("Seating " + customer + " at " + table);
-		hostGui.DoBringToTable(customer, table.tableNumber);
-
-	}
+	
 
 	//utilities
 
@@ -148,33 +92,13 @@ public class CookAgent extends Agent {
 		return hostGui;
 	}
 
-	private class Table {
-		CustomerAgent occupiedBy;
-		int tableNumber;
-
-		Table(int tableNumber) {
-			this.tableNumber = tableNumber;
-		}
-
-		void setOccupant(CustomerAgent cust) {
-			occupiedBy = cust;
-		}
-
-		void setUnoccupied() {
-			occupiedBy = null;
-		}
-
-		CustomerAgent getOccupant() {
-			return occupiedBy;
-		}
-
-		boolean isOccupied() {
-			return occupiedBy != null;
-		}
-
-		public String toString() {
-			return "table " + tableNumber;
-		}
+	private class Order {
+		//STUB
 	}
+	
+	private class Food {
+		//STUB
+	}
+	
 }
 
